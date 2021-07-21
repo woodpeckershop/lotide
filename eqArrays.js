28170804e4ed2f3cdd45e51606af2b5c3f9560bd
Chris <chris.sun5567@email.com>
2021-07-21 05:13:00 +0000

    first commit

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+function eqArrays(arrayA, arrayB) {
+  for (let i = 0; i < arrayA.length; i++) {
+    if (arrayA[i] !== arrayB[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    //return undefined;
+    console.log(`👍 👍 👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    //return 'incorrect assertion'
+    console.log(`👎 👎 👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
