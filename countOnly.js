5620195cf9433d6ddb9ff95a84010f022ce19d9c
Chris <chris.sun5567@email.com>
2021-07-22 00:19:12 +0000

    alternative way

--- countOnly.js
+++ countOnly.js
@@ -43,12 +43,15 @@ const countOnly = function (allItems, itemsToCount) {
     // increment the counter for each item:
     //   set a property with that string key to:
     //     the value that was already there (or zero if nothing there) with 1 added to it.
-    if (results[item]) {
-      results[item] += 1;
-    } else {
-      results[item] = 1;
+
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+      //console.log(item);
     }
-    //console.log(item);
   }
 
   return results;
@@ -73,7 +76,7 @@ const result1 = countOnly(firstNames, {
   Agouhanna: false,
 });
 
-//console.log(result1);
+console.log(result1);
 
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
