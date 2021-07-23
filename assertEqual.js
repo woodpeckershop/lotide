71d5624279b7c3f18b7478542ea9a1bf8f9bd4da
Chris <chris.sun5567@email.com>
2021-07-23 02:33:09 +0000

    1st commit

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,9 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function (actual, expected) {
-    if (actual === expected) {
-        //return undefined;
-        console.log(`👍 👍 👍 Assertion Passed: ${actual} === ${expected}`);
-    } else {
-        //return 'incorrect assertion'
-        console.log(`👎 👎 👎 Assertion Failed: ${actual} !== ${expected}`);
-    }
+  if (actual === expected) {
+    //return undefined;
+    console.log(`👍 👍 👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    //return 'incorrect assertion'
+    console.log(`👎 👎 👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
---
+++ findkey.js
@@ -0,0 +1,32 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    //return undefined;
+    console.log(`👍 👍 👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    //return 'incorrect assertion'
+    console.log(`👎 👎 👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function findKey(movieObj, callback) {
+  for (let key in movieObj) {
+    const bool = callback(movieObj[key]);
+    if (bool) {
+      return key;
+    }
+  }
+}
+
+const result = findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 2
+); // => "noma"
+
+assertEqual(result, "noma");
