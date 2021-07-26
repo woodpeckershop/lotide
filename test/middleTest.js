ff102c22046951d930e0000bcbaf35aa5487f322
Chris <chris.sun5567@email.com>
2021-07-26 21:16:20 +0000

    mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,25 @@
-const assertArraysEqual = require('../assertArraysEqual');
+
 const middle = require('../middle');
-const eqArrays = require('../eqArrays');
 
 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+const assert = require('chai').assert;
+
+describe("#middle", () => {
+  it("returns [3] for [1, 2, 3,4,5]", () => {
+    assert.deepEqual(middle([1, 2, 3,4,5]), [3]);
+  });
+
+  it("returns '[3,4]' for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]); 
+  });
+
+  
+  it("returns '[]' for [1]", () => {
+    assert.deepEqual(middle([1]), []); 
+  });
+
+  
+  it("returns '[]' for [1,2]", () => {
+    assert.deepEqual(middle([1,2]), []); 
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,26 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+
 
 // Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs");
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs");
+
+
+const assert = require('chai').assert;
+
+describe("#tail", () => {
+  it("returns 2 for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]).length, 2);
+  });
+  
+  it("returns '['Lighthouse', 'Labs']' for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"])[0], 'Lighthouse');
+  });
+
+});
